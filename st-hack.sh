ce4ecd34379c8277938e561f52b6889a5de3f292
sibertek-hd <63284238+sibertek-hd@users.noreply.github.com>
2020-06-08 09:52:28 +0300

    Add files via upload

---
+++ st-hack.sh
@@ -0,0 +1,14 @@
+clear 
+
+cowsay -f ghostbusters HACKED | lolcat
+echo
+figlet -f big ST HACK | lolcat
+echo 'İnstagram Kullanıcı Adı:'
+read
+max=10000000
+for (( i=2; i <= $max; i++))
+do
+echo -e "\e[92m\e[5m HACKED BY SİBERTEK"
+termux-open-url https://www.youtube.com/channel/UCnA-J-CqRpAUshewtsZQuCg
+done
+bash instagram_hack.sh
